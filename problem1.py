a71181a75fd0cd3dcaa66f35776fdef518b57796
Школьник01 <Kids01@edu.misis.ru>
2024-03-16 11:52:17 +0300

    Добавлен файл problem1.py

---
+++ problem1.py
@@ -0,0 +1,61 @@
+import csv
+
+def get_row(row):
+    """"Считывает строку и выдаёт обработанный словарь
+
+    Параметры:
+    row -- строка
+    """
+    return {"streams": int(row[0]), "artist_name": row[1], "track_name": row[2],"date": row[3]}
+
+def read_table(filename):
+    """Возвращает лист из словарей обработанной таблицы
+
+    Параметры:
+    filename -- имя файла
+    """
+    table = []
+    header = True
+    with open(filename, "r", encoding="utf8") as f:
+        for row in csv.reader(f, delimiter="?"):
+            if header:
+                header = False
+                continue
+            table.append(get_row(row))
+    return table
+
+def find_in_table(table, name):
+    """Ищет артиста по имени и выдаёт про него информацию
+
+    Параметры:
+    table -- основная таблица
+    name -- имя артиста
+    """
+    for row in table:
+        if row["artist_name"] == name:
+            return row
+
+def write_table(table, name):
+    """Создаёт таблицу со столбцами track_name, streams, date для одного артиста
+
+    Параметры:
+    table -- основная таблица
+    name -- имя артиста
+    """
+    artist_row = find_in_table(table, name)
+    track_name, streams, date = artist_row["track_name"], artist_row["streams"], artist_row["date"]
+
+    with open("songs_artst.csv", "w", encoding="utf8", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["track_name", "streams", "date"])
+        writer.writerow([track_name, streams, date])
+
+
+if __name__ == "__main__":
+    name = input("Введите имя исполнителя: ")
+    table = read_table("songs.txt")
+    if find_in_table(table, name):
+        write_table(table, name)
+        print("Результат записан в файл songs_artst.csv")
+    else:
+        print("Такого исполнителя не удалось найти.")
